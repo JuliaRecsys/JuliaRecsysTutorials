f6e792bc77d24d69d261619aac5e3d1dfb661c9a
Filipe Braida <filipebraida@gmail.com>
2021-05-28 12:42:13 -0300

    change notebook content

--- notebooks/00-first_steps.jl
+++ notebooks/00-first_steps.jl
@@ -20,13 +20,21 @@ O ambiente JuliaRecsys possui um conjunto de ferramentas para trabalhar com Sist
 
 No caso da filtragem colaborativa, o pacote Persa.jl é a base de todos os outros. Nele é contido um conjunto de interfaces básicas para todos os demais pacotes.
 
-## Analisando um Dataset
+## Funcionamento Básico
 
-O DatasetsCF.jl possui um conjunto de datasets para Sistema de Recomendação. Ele já possui a interação para o Persa.jl, assim, deixando transparente o seu carregamento para o usuário.
+A ideia da filtragem colaborativa é utilizar os padrões das avaliações feitas pelo usuário sobre os item para a realizar a predição dos seus gostos para a realização da recomendação. Desta forma, podemos representar a filtragem colaborativa através de uma matriz de usuário-item.
 
-Primeiramente iremos carregar o dataset MovieLens.
+| _        |  Item #1 |  Item #2 |  Item #3 |
+|:---------|:--------:|:--------:|:--------:|
+|  User #1 |    4     |          |    2     |
+|  User #2 |    4     |    5     |    3     |
+|  User #3 |    3     |    4     |          |
+|  User #4 |          |    5     |    4     |
 
 
+O Persa.jl define várias interfaces para manipular essa matriz. Contudo, o funcionamento interno não precisa ser necessariamente armazenado como uma matriz, visto que é muito comum que os conjuntos de dados possuam uma esparsidade muito alta.
+
+Iremos utilizar o DatasetsCF.jl para carregar o dataset MovieLens 100k.
 """
 
 # ╔═╡ 6df6d23c-46c2-4775-b269-c817b48e2d6d
@@ -34,70 +42,49 @@ dataset = DatasetsCF.MovieLens()
 
 # ╔═╡ feb8db18-f6eb-470a-b6fe-ff423a43bad2
 md"""
-Agora será realizado exibido o histograma de notas desse dataset
+Podemos navegar no dataset como se fosse uma navegação da matriz usuário-item. Iremos obter a nota do usuário 1 para o item 1.
 """
 
 # ╔═╡ cc807b5a-34fb-4ea4-99f4-f3b20cd9605e
-begin 
-	ratings = []
+rating = dataset[1,1]
 
-	for (u,v,r) in dataset
-		push!(ratings, r + 0)
-	end
-end
-
-# ╔═╡ 55e6cb45-fc5e-4df7-ae53-47dd9419aaf4
-histogram(ratings, bins=5, normalize=true, title = "Movielens 100k Ratings", xlabel="Ratings", ylabel="Quantity (%)", legend=false)
-
-# ╔═╡ 6594457c-7ca3-4e28-b80f-3aea147ba2a4
+# ╔═╡ 137adc82-3f52-4042-b460-ca626ab1749d
 md"""
-## Definindo o modelo preditivo
-Será criado um modelo que irá sortear aleatoriamente o valor da nota para qualquer usuário sobre um item.
+Como foi falado, a esparsidade dessa matriz é alta. É comum que o usuário não explicitou uma avaliação para aquele item. 
 
-Será necessário definir o modelo
+Segue um exemplo desse caso.
 """
 
-# ╔═╡ 37a964a6-4658-488d-b6c5-5e1f844be660
-begin 
-	struct RandomModel{T} <: Persa.Model{T}
-		preference::Persa.Preference{T}
-		users::Int
-		items::Int
-	end
-	
-	RandomModel(dataset::Persa.Dataset) = RandomModel(dataset.preference, Persa.users(dataset), Persa.items(dataset))
-end
+# ╔═╡ 29a262eb-a890-4b81-a1f6-636405ca12b6
+missing_rating = dataset[150,131]
 
-# ╔═╡ 87597c4f-4efa-46d7-9389-41eb1c2b5884
-Persa.predict(model::RandomModel, user::Int, item::Int) = rand(model.preference.possibles)
+# ╔═╡ 55e6cb45-fc5e-4df7-ae53-47dd9419aaf4
+md"""
+Também é possível utilizar os mecanismos de slice para obter as notas do usuário ou do item.
 
-# ╔═╡ 51fc82de-2f1b-403b-8a40-9b54260d919b
-model = RandomModel(dataset)
+No caso abaixo, iremos pegar todas as avaliações do usuário 1.
+"""
 
-# ╔═╡ fd667702-d282-4b5f-bd63-98fce091b530
-typeof(model[1,1])
+# ╔═╡ c84c72de-a86b-4b73-b361-7cff738bf55b
+user_ratings = dataset[1,:]
 
-# ╔═╡ 675780a5-2d10-4c7b-8e5c-29bd0381c359
-begin
-	error = 0
-	
-	for (u,v,r) in dataset
-		error += abs(model[u,v] - r)
-	end
-	
-	mae = error ./ length(dataset)
-end
+# ╔═╡ 0686b19d-654b-4b6c-b185-298831893190
+md"""
+Podemos pegar também as avaliações dos usuário 1 à 100 do item 5.
+"""
+
+# ╔═╡ a4f303c4-e764-4247-944f-3e4e018aab7d
+item_ratings = dataset[1:100, 5]
 
 # ╔═╡ Cell order:
-# ╠═4605da8e-bf1c-11eb-1be1-13b99165b934
+# ╟─4605da8e-bf1c-11eb-1be1-13b99165b934
 # ╟─6f37c1e5-e90a-445f-bf42-a69ce48dafe3
 # ╠═6df6d23c-46c2-4775-b269-c817b48e2d6d
 # ╟─feb8db18-f6eb-470a-b6fe-ff423a43bad2
 # ╠═cc807b5a-34fb-4ea4-99f4-f3b20cd9605e
-# ╠═55e6cb45-fc5e-4df7-ae53-47dd9419aaf4
-# ╟─6594457c-7ca3-4e28-b80f-3aea147ba2a4
-# ╠═37a964a6-4658-488d-b6c5-5e1f844be660
-# ╠═87597c4f-4efa-46d7-9389-41eb1c2b5884
-# ╠═51fc82de-2f1b-403b-8a40-9b54260d919b
-# ╠═fd667702-d282-4b5f-bd63-98fce091b530
-# ╠═675780a5-2d10-4c7b-8e5c-29bd0381c359
+# ╟─137adc82-3f52-4042-b460-ca626ab1749d
+# ╠═29a262eb-a890-4b81-a1f6-636405ca12b6
+# ╟─55e6cb45-fc5e-4df7-ae53-47dd9419aaf4
+# ╠═c84c72de-a86b-4b73-b361-7cff738bf55b
+# ╟─0686b19d-654b-4b6c-b185-298831893190
+# ╠═a4f303c4-e764-4247-944f-3e4e018aab7d
